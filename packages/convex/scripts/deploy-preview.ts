7380c7c039e125711accb127f94b1b6b58363479
Alexander Johansson <alexander@n1s.se>
2026-01-07 23:41:57 +0100

    .

--- packages/convex/scripts/deploy-preview.ts
+++ packages/convex/scripts/deploy-preview.ts
@@ -93,7 +93,7 @@ const cmds: Record<typeof env.VERCEL_ENV, () => Promise<void>> = {
 
     const convexEnvVarName = "\\$VITE_CONVEX_URL";
     await handleZodError(
-      $`npx convex deploy --preview-create ${env.VERCEL_GIT_COMMIT_REF} --cmd-url-env-var-name VITE_CONVEX_URL --cmd "echo VITE_CONVEX_URL=${convexEnvVarName} >> ${os.tmpdir()}/env.txt"`,
+      $`npx convex deploy --preview-create ${env.VERCEL_GIT_COMMIT_REF} --cmd-url-env-var-name VITE_CONVEX_URL --cmd "echo VITE_CONVEX_URL=\\\$VITE_CONVEX_URL >> ${os.tmpdir()}/env.txt"`,
     );
     const envFile = join(os.tmpdir(), "env.txt");
     const envContent = fs.readFileSync(envFile, "utf8");
