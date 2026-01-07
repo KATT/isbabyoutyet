94aaf07bf5c089dd16dee91fbdf56cce8480996f
Alexander Johansson <alexander@n1s.se>
2026-01-07 23:58:43 +0100

    .

--- packages/convex/scripts/deploy-preview.ts
+++ packages/convex/scripts/deploy-preview.ts
@@ -94,7 +94,7 @@ const cmds: Record<typeof env.VERCEL_ENV, () => Promise<void>> = {
     cd(convexPackageDir);
 
     const envFile = path.join(os.tmpdir(), "VITE_CONVEX_URL.txt");
-    const cmd = `echo \\$VITE_CONVEX_URL >> ${envFile}`;
+    const cmd = `echo \\\\$VITE_CONVEX_URL >> ${envFile}`;
     await handleZodError(
       $`npx convex deploy --preview-create ${env.VERCEL_GIT_COMMIT_REF} --cmd-url-env-var-name VITE_CONVEX_URL --cmd ${cmd}`,
     );
